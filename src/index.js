918cd72d5cbacae4424a20d99d01bfcb6086b637
ajkelso <ajkelso@gmail.com>
2021-01-01 21:42:36 -0500

    commit tree aa0df41373f68c775f00427e01c24668aa376c58

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function renderImg(json) {
+    const imgs = json.message
+    const imgContainer = document.getElementById("dog-image-container")
+    imgs.forEach(image => {
+        const img = document.createElement('img')
+        img.src = image
+        imgContainer.appendChild(img) 
+    });
+}
+
+function fetchImages() {
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImg(json))
+}
+
